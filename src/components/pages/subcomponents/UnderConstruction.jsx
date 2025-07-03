8817071d5805507e75e529d1e1f17a858808575c
amusictheorist <a_millaresthomson@outlook.com>
2025-07-03 13:59:50 -0400

    create 'under construction' page

---
+++ src/components/pages/subcomponents/UnderConstruction.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const UnderConstruction = () => {
+  return (
+    <div className="mt-8 text-center text-gray-600">
+      <h2 className="text-2xl font-serif font-semibold mb-4">Page Under Construction</h2>
+      <p className="text-lg font-serif">
+        We're still working on this musician's biography. Please check back later!
+      </p>
+    </div>
+  );
+};
+
+export default UnderConstruction;
